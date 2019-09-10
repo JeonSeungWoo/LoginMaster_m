eb674ce57deea8bed27e213da626adcd6b8f4616
21class-007 <21class-007@21class-007-PC>
2019-09-10 13:43:02 +0900

    DataSource 까지 완성

--- PostgreSQLDBConnercion/src/test/java/org/spring/woo/DSTest.java
+++ PostgreSQLDBConnercion/src/test/java/org/spring/woo/DSTest.java
@@ -1,29 +1,29 @@
 package org.spring.woo;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
-public class DBTest {
+public class DSTest {
 
+	@Inject
+	private DataSource ds;
+	
 	
 	@Test
-	public void DBConnectionTest() throws Exception {
-	Class.forName("org.postgresql.Driver");
-	Connection con = DriverManager.getConnection(
-	"jdbc.url=jdbc:postgresql://localhost:5432/postgres",
-	"postgres",
-	"1234");
-	System.out.println(con);
-	con.close();
+	public void dsTest()throws Exception{
+		Connection con = ds.getConnection();
+		System.out.println(con);
+		con.close();
+		
 	}
 	
-
 }
